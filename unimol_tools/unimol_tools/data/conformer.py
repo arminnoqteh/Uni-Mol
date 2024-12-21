18d7ad7f477a43e9eb2d548ef85052747e012228
arminnoqteh <arminnoqteh@gmail.com>
2024-12-21 14:50:14 +0330

    gotten rid of multi

--- unimol_tools/unimol_tools/data/conformer.py
+++ unimol_tools/unimol_tools/data/conformer.py
@@ -369,15 +369,30 @@ class UniMolV2Feature(object):
 
     def transform(self, smiles_list):
 
-        with Pool(processes=4) as pool:
-            logger.info("Start generating conformers...")
-            inputs = []
-            for item in tqdm(pool.imap(self.single_process, smiles_list)):
-                inputs.append(item)
-        # failed_cnt = np.mean([(item['src_coord']==0.0).all() for item in inputs])
-        # logger.info('Succeeded in generating conformers for {:.2f}% of molecules.'.format((1-failed_cnt)*100))
-        # failed_3d_cnt = np.mean([(item['src_coord'][:,2]==0.0).all() for item in inputs])
-        # logger.info('Succeeded in generating 3d conformers for {:.2f}% of molecules.'.format((1-failed_3d_cnt)*100))
+        # with Pool(processes=4) as pool:
+        #     logger.info("Start generating conformers...")
+        #     inputs = []
+        #     for item in tqdm(pool.imap(self.single_process, smiles_list)):
+        #         inputs.append(item)
+
+        logger.info("Start generating conformers without multiprocessing...")
+        inputs = []
+        for item in tqdm([self.single_process(smiles) for smiles in smiles_list]):
+            inputs.append(item)
+        failed_cnt = np.mean([(item["src_coord"] == 0.0).all() for item in inputs])
+        logger.info(
+            "Succeeded in generating conformers for {:.2f}% of molecules.".format(
+                (1 - failed_cnt) * 100
+            )
+        )
+        failed_3d_cnt = np.mean(
+            [(item["src_coord"][:, 2] == 0.0).all() for item in inputs]
+        )
+        logger.info(
+            "Succeeded in generating 3d conformers for {:.2f}% of molecules.".format(
+                (1 - failed_3d_cnt) * 100
+            )
+        )
         return inputs
 
 
