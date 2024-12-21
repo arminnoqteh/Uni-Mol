adb38b4ad149bfa61c875d58c40c035d3b070df0
arminnoqteh <arminnoqteh@gmail.com>
2024-12-21 16:35:44 +0330

    set multiprocessing context to 'forkserver' and restore parallel processing for conformer generation

--- unimol_tools/unimol_tools/data/conformer.py
+++ unimol_tools/unimol_tools/data/conformer.py
@@ -22,7 +22,7 @@ from numba import njit
 import multiprocessing
 
 # set context for multiprocessing
-# multiprocessing.set_start_method("forkserver", force=True)
+multiprocessing.set_start_method("forkserver", force=True)
 
 from ..utils import logger
 from ..config import MODEL_CONFIG
@@ -373,30 +373,30 @@ class UniMolV2Feature(object):
 
     def transform(self, smiles_list):
 
-        # with Pool(processes=4) as pool:
-        #     logger.info("Start generating conformers...")
-        #     inputs = []
-        #     for item in tqdm(pool.imap(self.single_process, smiles_list)):
-        #         inputs.append(item)
+        with Pool() as pool:
+            logger.info("Start generating conformers...")
+            inputs = []
+            for item in tqdm(pool.imap(self.single_process, smiles_list)):
+                inputs.append(item)
 
-        inputs = []
-        for item in tqdm([self.single_process(smiles) for smiles in smiles_list]):
-            inputs.append(item)
+        # inputs = []
+        # for item in tqdm([self.single_process(smiles) for smiles in smiles_list]):
+        #     inputs.append(item)
 
-        failed_cnt = np.mean([(item["src_coord"] == 0.0).all() for item in inputs])
-        logger.info(
-            "Succeeded in generating conformers for {:.2f}% of molecules.".format(
-                (1 - failed_cnt) * 100
-            )
-        )
-        failed_3d_cnt = np.mean(
-            [(item["src_coord"][:, 2] == 0.0).all() for item in inputs]
-        )
-        logger.info(
-            "Succeeded in generating 3d conformers for {:.2f}% of molecules.".format(
-                (1 - failed_3d_cnt) * 100
-            )
-        )
+        # failed_cnt = np.mean([(item["src_coord"] == 0.0).all() for item in inputs])
+        # logger.info(
+        #     "Succeeded in generating conformers for {:.2f}% of molecules.".format(
+        #         (1 - failed_cnt) * 100
+        #     )
+        # )
+        # failed_3d_cnt = np.mean(
+        #     [(item["src_coord"][:, 2] == 0.0).all() for item in inputs]
+        # )
+        # logger.info(
+        #     "Succeeded in generating 3d conformers for {:.2f}% of molecules.".format(
+        #         (1 - failed_3d_cnt) * 100
+        #     )
+        # )
         return inputs
 
 
